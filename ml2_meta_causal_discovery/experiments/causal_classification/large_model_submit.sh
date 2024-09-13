aedd4a95610a75b1ed91504935900edb4133d2fe
anish144 <anishdhir144@gmail.com>
2024-09-13 16:30:35 +0100

    about to run neural net runs

--- ml2_meta_causal_discovery/experiments/causal_classification/large_model_submit.sh
+++ ml2_meta_causal_discovery/experiments/causal_classification/large_model_submit.sh
@@ -22,5 +22,5 @@ do
     IFS="," read -r NE ND DM DF NH DS MT <<< "$i"
 
     # Submit the job with the appropriate variables
-    qsub -q hx -v "NE=$NE,ND=$ND,DM=$DM,DF=$DF,NH=$NH,DS=$DS,MT=$MT" -N "MT${$MT}_NE${NE}_ND${ND}_DM${DM}_DF${DF}_NH${NH}_DS${DS}" /gpfs/home/ad6013/Research/CausalStructureNeuralProcess/ml2_meta_causal_discovery/experiments/causal_classification/hx1_large_model.pbs
+    qsub -q hx -v "NE=$NE,ND=$ND,DM=$DM,DF=$DF,NH=$NH,DS=$DS,MT=$MT" -N "MT${MT}_NE${NE}_ND${ND}_DM${DM}_DF${DF}_NH${NH}_DS${DS}" /gpfs/home/ad6013/Research/CausalStructureNeuralProcess/ml2_meta_causal_discovery/experiments/causal_classification/hx1_large_model.pbs
 done
