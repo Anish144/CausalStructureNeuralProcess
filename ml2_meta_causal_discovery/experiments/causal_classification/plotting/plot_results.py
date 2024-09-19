a3c49eb1206f8e042bd9414b0dab8a2e0875acc5
Anish144 <anishdhir144@gmail.com>
2024-09-19 21:23:15 +0100

    results done for plotting

--- ml2_meta_causal_discovery/experiments/causal_classification/plotting/plot_results.py
+++ ml2_meta_causal_discovery/experiments/causal_classification/plotting/plot_results.py
@@ -9,16 +9,41 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def load_baseline_results(work_dir: Path, baseline_file: list):
-    result_file = work_dir / "experiments" / "causal_classification" / "baseline_results"
-    full_results = {}
-    for file in baseline_file:
-        if file is None:
-            continue
-        with open(result_file / f"{file}.json", "r") as f:
-            results = json.load(f)
-        full_results[file] = results
-    return full_results
+
+def load_baseline_results(work_dir: Path, baseline_file: list, data_name: str):
+    all_baselines = {}
+    for baseline in baseline_file:
+        result_file = work_dir / "experiments" / "causal_classification" / "baseline_results"
+        baseline_full_results = {
+            "auc": [],
+            "e_f1": [],
+            "e_shd": [],
+            "log_prob": [],
+        }
+        baseline_result_folder = result_file / baseline / f"{data_name}_test"
+        for final_result_folder in baseline_result_folder.iterdir():
+
+            if baseline == "bayesdag":
+                for result_file in final_result_folder.iterdir():
+                    # Read the results
+                    with open(result_file, "r") as f:
+                        results = json.load(f)
+            elif baseline == "dibs":
+                with open(final_result_folder, "r") as f:
+                    results = json.load(f)
+
+            if baseline == "bayesdag":
+                baseline_full_results["log_prob"].append(results['test_data']["log_prob"])
+                baseline_full_results["auc"].append(results['test_data']["auc"])
+                baseline_full_results["e_f1"].append(results['test_data']["orientation_fscore"])
+                baseline_full_results["e_shd"].append(results['test_data']["shd"])
+            elif baseline == "dibs":
+                baseline_full_results["log_prob"].append(results["log_prob"])
+                baseline_full_results["e_f1"].append(results["F1"])
+                baseline_full_results["e_shd"].append(results["SHD"])
+                baseline_full_results["auc"].append(results["AUC"])
+        all_baselines[baseline] = baseline_full_results
+    return all_baselines
 
 
 def load_model_results(work_dir: Path, model_files: list, data_name: str):
@@ -78,7 +103,7 @@ def plot_results(results: dict, model_key: dict, data_name: str):
         ax.get_legend().remove()
 
         # Save each plot as a high-resolution image
-        output_path = Path(__file__).absolute().parent / f'{data_name}_{var}_Boxplot.png'
+        output_path = Path(__file__).absolute().parent / f'Baseline_{data_name}_{var}_Boxplot.png'
         plt.savefig(output_path, format='png', dpi=300, bbox_inches='tight')
 
         # Show the plot
@@ -92,11 +117,10 @@ def main(
     model_key: dict,
     data_name: str,
 ):
-    baseline_results = load_baseline_results(work_dir, baseline_files)
+    baseline_results = load_baseline_results(work_dir, baseline_files, data_name)
     model_results = load_model_results(work_dir, model_files, data_name)
     # Combine two dicts
     results = {**baseline_results, **model_results}
-
     # Function to clean and convert string to numpy array
     def clean_and_convert(arr_string):
         return np.array(arr_string).astype(float)
@@ -133,52 +157,67 @@ def main(
 if __name__ == "__main__":
     work_dir = Path(__file__).absolute().parent.parent.parent.parent
 
-    # Need this to load the results
-    data_files = [
+    data_name_list = [
         "neuralnet_20var_ER20",
         "neuralnet_20var_ER40",
         "neuralnet_20var_ER60",
-        "neuralnet_20var_ERL20U60",
+        # "neuralnet_20var_ERL20U60",
     ]
 
-    # baseline_file_1 = "gplvm_er20_bayesdag"
-    baseline_file_1 = None
+    # Need this to load the results
+    # data_files = [
+        # "neuralnet_20var_ER20",
+        # "neuralnet_20var_ER40",
+        # "neuralnet_20var_ER60",
+        # "neuralnet_20var_ERL20U60",
+    # ]
 
-    model_1 = "transformer_neuralnet_20var_ER20_NH8_NE4_ND4_DM512_DF1024"
-    model_2 = "transformer_neuralnet_20var_ER40_NH8_NE4_ND4_DM512_DF1024"
-    model_3 = "transformer_neuralnet_20var_ER60_NH8_NE4_ND4_DM512_DF1024"
-    model_4 = "transformer_neuralnet_20var_ERL20U60_NH8_NE4_ND4_DM512_DF1024"
-    model_5 = "probabilistic_neuralnet_20var_ER20_NH8_NE4_ND4_DM512_DF1024"
-    model_6 = "probabilistic_neuralnet_20var_ER40_NH8_NE4_ND4_DM512_DF1024"
-    model_7 = "probabilistic_neuralnet_20var_ER60_NH8_NE4_ND4_DM512_DF1024"
-    model_8 = "probabilistic_neuralnet_20var_ERL20U60_NH8_NE4_ND4_DM512_DF1024"
+    baseline_model_1 = "bayesdag"
+    baseline_model_2 = "dibs"
+    # baseline_file_1 = None
 
-    model_key = {
-        model_1: "avici_ER20",
-        model_2: "avici_ER40",
-        model_3: "avici_ER60",
-        model_4: "avici_ERL20U60",
-        model_5: "prob_ER20",
-        model_6: "prob_ER40",
-        model_7: "prob_ER60",
-        model_8: "prob_ERL20U60",
-    }
+    # model_2 = "transformer_neuralnet_20var_ER40_NH8_NE4_ND4_DM512_DF1024"
+    # model_3 = "transformer_neuralnet_20var_ER60_NH8_NE4_ND4_DM512_DF1024"
+    # model_4 = "transformer_neuralnet_20var_ERL20U60_NH8_NE4_ND4_DM512_DF1024"
+    # model_5 = "probabilistic_neuralnet_20var_ER20_NH8_NE4_ND4_DM512_DF1024"
+    # model_6 = "probabilistic_neuralnet_20var_ER40_NH8_NE4_ND4_DM512_DF1024"
+    # model_7 = "probabilistic_neuralnet_20var_ER60_NH8_NE4_ND4_DM512_DF1024"
+    # model_8 = "probabilistic_neuralnet_20var_ERL20U60_NH8_NE4_ND4_DM512_DF1024"
 
     baseline_files = [
-        baseline_file_1
-    ]
-    model_files = [
-        model_1,
-        model_2,
-        model_3,
-        model_4,
-        model_5,
-        model_6,
-        model_7,
-        model_8,
+        baseline_model_1,
+        baseline_model_2,
     ]
 
-    for data in data_files:
+
+    for data in data_name_list:
+        model_1 = f"transformer_{data}_NH8_NE4_ND4_DM512_DF1024"
+        model_2 = f"probabilistic_{data}_NH8_NE4_ND4_DM512_DF1024"
+        model_3 = f"probabilistic_neuralnet_20var_ERL20U60_NH8_NE4_ND4_DM512_DF1024"
+
+        model_key = {
+            model_1: "AVICI",
+            model_2: "BCNP",
+            model_3: "BCNP-ERL20U60",
+            baseline_model_1: "BayesDAG",
+            baseline_model_2: "DiBS",
+            # model_3: "avici_ER60",
+            # model_4: "avici_ERL20U60",
+            # model_5: "prob_ER20",
+            # model_6: "prob_ER40",
+            # model_7: "prob_ER60",
+            # model_8: "prob_ERL20U60",
+        }
+        model_files = [
+            model_1,
+            model_2,
+            model_3,
+            # model_4,
+            # model_5,
+            # model_6,
+            # model_7,
+            # model_8,
+        ]
         main(
             work_dir=work_dir,
             baseline_files=baseline_files,
