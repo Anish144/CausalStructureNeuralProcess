9d3c1afe92752fbcb5c43d862a7756bee6a48650
Anish144 <anishdhir144@gmail.com>
2024-09-28 12:16:46 +0100

    csvia ready

--- ml2_meta_causal_discovery/experiments/causal_classification/large_model_submit.sh
+++ ml2_meta_causal_discovery/experiments/causal_classification/large_model_submit.sh
@@ -10,17 +10,17 @@ param_list=(
     # "4,4,512,1024,8,neuralnet_20var_ER40,autoregressive",
     # "4,4,512,1024,8,neuralnet_20var_ER60,autoregressive",
     # "4,4,512,1024,8,neuralnet_20var_ERL20U60,autoregressive",
-    "4,4,512,1024,8,neuralnet_20var_ER20,transformer", # This model fits on compute
-    "4,4,512,1024,8,neuralnet_20var_ER40,transformer",
-    "4,4,512,1024,8,neuralnet_20var_ER60,transformer",
+    "4,4,512,1024,8,neuralnet_20var_ER20,autoregressive", # This model fits on compute
+    "4,4,512,1024,8,neuralnet_20var_ER40,autoregressive",
+    "4,4,512,1024,8,neuralnet_20var_ER60,autoregressive",
     # "4,4,512,1024,8,neuralnet_20var_ERL20U60,transformer",
     # "4,4,512,1024,8,linear_20var_ER20,probabilistic",
     # "4,4,512,1024,8,linear_20var_ER40,probabilistic",
     # "4,4,512,1024,8,linear_20var_ER60,probabilistic",
     # "4,4,512,1024,8,linear_20var_ERL20U60,probabilistic",
-    "4,4,512,1024,8,linear_20var_ER20,transformer",
-    "4,4,512,1024,8,linear_20var_ER40,transformer",
-    "4,4,512,1024,8,linear_20var_ER60,transformer",
+    "4,4,512,1024,8,linear_20var_ER20,autoregressive",
+    "4,4,512,1024,8,linear_20var_ER40,autoregressive",
+    "4,4,512,1024,8,linear_20var_ER60,autoregressive",
     # "4,4,512,1024,8,linear_20var_ERL20U60,transformer",
     # "4,4,512,1024,8,gplvm_neuralnet_20var_ERSFL20U60,probabilistic",
 )
