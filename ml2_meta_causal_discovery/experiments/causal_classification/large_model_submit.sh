65f7e084f6571a62ff0768850cd0ee54c300da79
Anish144 <anishdhir144@gmail.com>
2024-11-18 17:54:47 +0000

    change in num_samples in .sh file

--- ml2_meta_causal_discovery/experiments/causal_classification/large_model_submit.sh
+++ ml2_meta_causal_discovery/experiments/causal_classification/large_model_submit.sh
@@ -47,9 +47,9 @@ param_list=(
 #     "4,4,512,2048,16,gplvm_neuralnet_20var_ERSFL20U60,probabilistic,32,2,500", # RUN
 #     "4,4,512,2048,16,gplvm_neuralnet_20var_ERSFL20U60,transformer,32,2,500", # RUN
 #     "4,4,256,512,8,gplvm_neuralnet_20var_ERSFL20U60,autoregressive,8,5,500", # RUN
-    "4,4,512,1024,8,lowdata_neuralnet_20var_ER40,probabilistic,32,2,100",
-    "4,4,512,1024,8,lowdata_neuralnet_20var_ER40,transformer,32,2,100",
-    "4,4,256,512,8,lowdata_neuralnet_20var_ER40,autoregressive,8,2,100",
+    "4,4,512,1024,8,lowdata_neuralnet_20var_ER40,probabilistic,32,2,50",
+    "4,4,512,1024,8,lowdata_neuralnet_20var_ER40,transformer,32,2,50",
+    "4,4,256,512,8,lowdata_neuralnet_20var_ER40,autoregressive,8,2,50",
     # "4,4,512,4096,16,challenge_training,probabilistic,32,2,1000"
     # "4,4,1024,2048,16,challenge_training,probabilistic,32,2,1000"
     # "4,4,1024,4096,16,challenge_training,probabilistic,32,2,1000"
